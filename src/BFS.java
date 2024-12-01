dfa29efa37916dad42502d94d8e657256331a647
fusiontwo <yr0955@naver.com>
2024-12-02 00:25:38 +0900

    fix: add custom queue in BFS, custom stack in DFS

--- src/BFS.java
+++ src/BFS.java
@@ -6,22 +6,43 @@ import java.util.Queue;
 public class BFS {
     public void bfs(int[][] matrix, int s) {
         int nodesCount = matrix.length;
-
         boolean[] visited = new boolean[nodesCount];
-        Queue<Integer> queue = new LinkedList<>();
         List<String> result = new ArrayList<>();
 
-        visited[s] = true;
-        queue.add(s);
+        // 방법 1. Queue 클래스 활용
+//        Queue<Integer> queue = new LinkedList<>();
+//
+//        visited[s] = true;
+//        queue.add(s);
+//
+//        while (!queue.isEmpty()) {
+//            int closedNode = queue.poll();
+//            result.add(String.valueOf(closedNode + 1));
+//
+//            for (int i = 0; i < nodesCount; i++) {
+//                if (matrix[closedNode][i] == 1 && !visited[i]) {
+//                    visited[i] = true;
+//                    queue.add(i);
+//                }
+//            }
+//        }
 
-        while (!queue.isEmpty()) {
-            int closedNode = queue.poll();
+        // 방법 2. Queue 직접 구현
+        int[] queue = new int[nodesCount];
+        int head = 0;  // head: 큐의 값을 제거할 위치
+        int tail = -1;  // tail: 큐에 값을 추가할 위치
+
+        queue[++tail] = s;  // tail 을 1 증가시키고, 시작 정점 s를 저장
+        visited[s] = true;
+
+        while (head <= tail) {
+            int closedNode = queue[head++];  // head 위치의 값을 가져오고, head 를 1 증가시킴.
             result.add(String.valueOf(closedNode + 1));
 
             for (int i = 0; i < nodesCount; i++) {
                 if (matrix[closedNode][i] == 1 && !visited[i]) {
                     visited[i] = true;
-                    queue.add(i);
+                    queue[++tail] = i;  // tail 을 1 증가시키고, 정점 i를 저장
                 }
             }
         }
--- src/DFS.java
+++ src/DFS.java
@@ -6,13 +6,36 @@ public class DFS {
     public void dfs(int[][] matrix, int s) {
         int nodesCount = matrix.length;
         boolean[] visited = new boolean[nodesCount];
-        Stack<Integer> stack = new Stack<>();
         List<String> result = new ArrayList<>();
 
-        stack.push(s);
+        // 방법 1. Stack 클래스 활용
+//        Stack<Integer> stack = new Stack<>();
+//
+//        stack.push(s);
+//
+//        while (!stack.isEmpty()) {
+//            int closedNode = stack.pop();
+//
+//            if (!visited[closedNode]) {
+//                visited[closedNode] = true;
+//                result.add(String.valueOf(closedNode + 1));
+//
+//                for (int i = nodesCount - 1; i >= 0; i--) {
+//                    if (matrix[closedNode][i] == 1 && !visited[i]) {
+//                        stack.push(i);
+//                    }
+//                }
+//            }
+//        }
 
-        while (!stack.isEmpty()) {
-            int closedNode = stack.pop();
+        // 방법 2. Stack 직접 구현
+        int[] stack = new int[nodesCount];
+        int top = -1;  // top: stack 의 맨 위 위치를 표시함.
+
+        stack[++top] = s;  // top 을 1 증가시키고 stack[0]에 시작 정점 s 저장
+
+        while (top >= 0) {  // stack 이 비어있지 않을 때까지
+            int closedNode = stack[top--];  // top 위치의 값을 가져오고, top 을 1 감소시킴.
 
             if (!visited[closedNode]) {
                 visited[closedNode] = true;
@@ -20,7 +43,7 @@ public class DFS {
 
                 for (int i = nodesCount - 1; i >= 0; i--) {
                     if (matrix[closedNode][i] == 1 && !visited[i]) {
-                        stack.push(i);
+                        stack[++top] = i;  // top 을 1 증가시키고, 정점 i를 추가
                     }
                 }
             }
